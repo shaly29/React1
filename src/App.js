ee8e4703aef10a2864445dc5f2def7524b7a4650
shaly29 <nishalinithirukkumar2@gmail.com>
2023-12-28 19:39:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 
-// import './App.css';
-// import Button from './Button';
-// import Header from './Header';
-// import Content from './Content'
-// import Footer from './Footer'
-// // import Text from './Button'
-// function App() {
-//   return (
-//     <div>
-//       <Header/>
-//     <Content/>
-//     <Footer/>
-//     </div>
-//   );
-// }
+import './App.css';
+import Button from './Button';
+import Header from './Header';
+import Content from './Content'
+import Footer from './Footer'
+// import Text from './Button'
+function App() {
+  return (
+    <div>
+      <Header/>
+    <Content/>
+    <Footer/>
+    </div>
+  );
+}
 
 
 
